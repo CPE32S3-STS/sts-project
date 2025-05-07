35e5efe825ec2bad9d08b467f9a5d33e178b0d28
JemDeGuzman <qjedeguzman@tip.edu.ph>
2025-05-08 01:57:05 +0800

    BIG CONTENT UPDATE

---
+++ movies.js
@@ -0,0 +1,49 @@
+const movies = {
+    "ctrl-alt-dct": {
+      title: "Control Alt Deceit",
+      synopsis: "Ethan, skeptic in antivirus softwares, ignores his workmate’s warning and clicks a suspicious email link. That night, he finds his files locked by ransomware. As his files are held against him, a demand appears on his screen which forces him into a mysterious and unsettling encounter with the person behind it all. What follows is a tense confrontation that challenges Ethan’s understanding of digital safety and himself.",
+      director: "Ery Jay Pisalbon",
+      writer: "Ery Jay Pisalbon, Jay-ann Alorro",
+      editor: "Ralph Christian Detchosa",
+      actors: "Karl Josef Malaki, Jay-ann Alorro, Xander Galapia",
+      cameraman: "Liam Francis Calamba",
+      videoUrl: "videos/ctrl-alt-dct.mp4"
+    },
+    "romance-scam": {
+      title: "Romance? Scam?",
+      synopsis: "When John Lloyd stumbles upon a dating app called Affinity, he dives in with his best friend Jeremy just for fun. But what starts as casual swiping turns serious when he meets Sarah—a charming woman who quickly captures his heart. As their virtual romance deepens, Sarah begins asking for financial help, and despite Jeremy’s growing concerns, John Lloyd finds it hard to say no. Blinded by love, he ignores the red flags—until he uncovers a shocking truth that forces him to confront the deception and warn others before it’s too late.",
+      director: "Marc Joseph Dolores",
+      writer: "Marc Joseph Dolores, John Carlos Garcia",
+      editor: "Marc Joseph Dolores, James Mallanao",
+      actors: "Jeremy John Orlina, Johnlloyd Padrigano, John Carlos Garcia, Sarah Galit",
+      videoUrl: "videos/romance-scam.mp4"
+    },
+    "brooklyn99": {
+      title: "Brooklyn Nine-Nine",
+      synopsis: "A hilarious ensemble of detectives solve crimes and crack jokes in Brooklyn.",
+      director: "Dan Goor",
+      writer: "Michael Schur",
+      editor: "Cortney Carrillo",
+      actors: "Andy Samberg, Terry Crews, Stephanie Beatriz",
+      videoUrl: "videos/brooklyn99.mp4"
+    },
+    "spiderman": {
+      title: "Spider-Man: No Way Home",
+      synopsis: "Spider-Man faces multiversal mayhem in his most dangerous mission yet.",
+      director: "Jon Watts",
+      writer: "Chris McKenna, Erik Sommers",
+      editor: "Jeffrey Ford",
+      actors: "Tom Holland, Zendaya, Benedict Cumberbatch",
+      videoUrl: "videos/spiderman.mp4"
+    },
+    "now-you-see-me": {
+      title: "Now You See Me",
+      synopsis: "A team of illusionists pull off bank heists during their performances.",
+      director: "Louis Leterrier",
+      writer: "Ed Solomon, Boaz Yakin",
+      editor: "Robert Leighton",
+      actors: "Jesse Eisenberg, Mark Ruffalo, Woody Harrelson",
+      videoUrl: "videos/now-you-see-me.mp4"
+    }
+  };
+  

d257f48d53282e29e7e892595f69eb6e4ce5a09e
Jann-Moises-Nyll-De-Los-Reyes <109423979+akosinyll1404@users.noreply.github.com>
2025-05-09 13:11:49 +0800

    change movies js

--- movies.js
+++ movies.js
@@ -16,34 +16,28 @@ const movies = {
       writer: "Marc Joseph Dolores, John Carlos Garcia",
       editor: "Marc Joseph Dolores, James Mallanao",
       actors: "Jeremy John Orlina, Johnlloyd Padrigano, John Carlos Garcia, Sarah Galit",
+      cameraman:"Marc Joseph Dolores",
       videoUrl: "videos/romance-scam.mp4"
     },
-    "brooklyn99": {
-      title: "Brooklyn Nine-Nine",
-      synopsis: "A hilarious ensemble of detectives solve crimes and crack jokes in Brooklyn.",
-      director: "Dan Goor",
-      writer: "Michael Schur",
-      editor: "Cortney Carrillo",
-      actors: "Andy Samberg, Terry Crews, Stephanie Beatriz",
-      videoUrl: "videos/brooklyn99.mp4"
+    "friendly-fire": {
+      title: "Friendly Fire",
+      synopsis: "Friendly Fire illustrates the digital decomposition of a college friend group perpetrated in the person of Jhill, well-intentioned student, who blindly finds herself as the target of persistent cyberbullying under the disguise of friendly jokes. What starts off as playful banter quickly deteriorates into exclusion, anonymous harassment, and emotional manipulation in this all too real 'game', substantiated by group chats, memes, and hidden group threads. Jhill is left to contend with betrayal, mental anguish, and loneliness as the blurred lines between friendship and malice become increasingly impossible to navigate all while her so-called friends diminish the experience by laughing it off as 'just a joke'. Told through screen recordings and chat messages only, Friendly Fire is a compelling cautionary tale of today's digital world about the deadly consequences of digital trauma, and the silence of peers when witnessing or experiencing bullying.",
+      director: "Jhillian Millare Cabos",
+      writer: "John Rome Belocora, Jhilllian Millare Cabos, and Juliann Vincennt Quibral",
+      editor: "John Rome Belocora and Juliann Vincent Quibral",
+      actors: "Viktor Angelo Apuyan, John Rome Belocora, Jhillian Millare Cabos, Juliann Vincent Quibral, Jens Liam Vista, Arjay Soriano",
+      cameraman:"John Rome Belacora",
+      videoUrl: "videos/friendl-fire.mp4" 
     },
-    "spiderman": {
-      title: "Spider-Man: No Way Home",
-      synopsis: "Spider-Man faces multiversal mayhem in his most dangerous mission yet.",
-      director: "Jon Watts",
-      writer: "Chris McKenna, Erik Sommers",
-      editor: "Jeffrey Ford",
-      actors: "Tom Holland, Zendaya, Benedict Cumberbatch",
-      videoUrl: "videos/spiderman.mp4"
-    },
-    "now-you-see-me": {
-      title: "Now You See Me",
-      synopsis: "A team of illusionists pull off bank heists during their performances.",
-      director: "Louis Leterrier",
-      writer: "Ed Solomon, Boaz Yakin",
-      editor: "Robert Leighton",
-      actors: "Jesse Eisenberg, Mark Ruffalo, Woody Harrelson",
-      videoUrl: "videos/now-you-see-me.mp4"
+    "ctrl-steal": {
+      title: "Ctrl + Steal",
+      synopsis: "A group of friends gather to enjoy gaming on an expensive device, reveling in their shared fun. Gabrielle, the outsider, watches with growing envy, longing for the luxury they take for granted. Determined to claim that same experience for himself, he turns to pirated software—unaware that this reckless choice will lead to his downfall.",
+      director: "Audrick Zander Cuadra",
+      writer: "Dylan 'Baby' James Dejoras",
+      editor: "Lance Mariano Montojo",
+      actors: "Gabrielle Dela Cruz, Angelo Luis Cu, Lance Montojo",
+      cameraman:"Angelo Luis Cu",
+      videoUrl: "videos/ctrl-steal.mp4"
     }
   };
   
